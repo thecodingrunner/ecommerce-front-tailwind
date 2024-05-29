b372e771e10eaf21459b5fb1d8e103aa7a81d756
thecodingrunner <finn.moffett@gmail.com>
2024-05-29 22:28:36 +0100

    Added vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { CartContextProvider } from "@/components/CartContext";
 import "@/styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
     <CartContextProvider>
+      <SpeedInsights/>
       <Component {...pageProps} />
     </CartContextProvider>
   );
