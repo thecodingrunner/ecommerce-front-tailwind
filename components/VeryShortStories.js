410db63270bb838439d468259a2e379369087144
thecodingrunner <finn.moffett@gmail.com>
2024-05-29 20:30:27 +0100

    Vercel deployment

--- components/VeryShortStories.js
+++ components/VeryShortStories.js
@@ -6,15 +6,15 @@ import follower from '../public/follower_nobg.png'
 import Image from "next/image";
 
 export default function Hero() {
-    console.log(VeryShortStories[6].image.src)
+    console.log(dream)
     console.log(VeryShortStories[1].image.src)
     return (
         <article className="hero relative overflow-hidden">
             <h1 className="h1 z-10 text-center">Very Short Stories</h1>
             <Link className="btn-primary z-10 text-2xl" href={'/very-short-stories'}>Explore</Link>
-            <Image src={dream.src} alt='dream' className="h-full absolute left-3/4 -bottom-6 lg:block hidden"/>
-            <Image src={follower.src} alt="follower" className="h-full absolute left-2/6 -bottom-3 lg:block hidden"/>
-            <Image src={fury.src} alt="fury" className="h-full absolute lg:left-3 -bottom-0"/>
+            <Image src={dream.src} alt='dream' height={dream.height} width={dream.width/2.2} className="h-full absolute left-2/3 -bottom-6 lg:block hidden"/>
+            <Image src={follower.src} alt="follower" height={follower.height} width={follower.width} className="h-full absolute left-2/6 -bottom-3 lg:block hidden"/>
+            <Image src={fury.src} alt="fury" height={fury.height} width={fury.width} className="h-full absolute lg:left-3 -bottom-0"/>
             {/* <Image src={VeryShortStories[1].image.src} className="h-full absolute left-3/4 -bottom-6 lg:block hidden"/>
             <Image src={VeryShortStories[3].image.src} className="h-full absolute left-2/6 -bottom-3 lg:block hidden"/>
             <Image src={VeryShortStories[6].image.src} className="h-full absolute lg:left-3 -bottom-0"/> */}
