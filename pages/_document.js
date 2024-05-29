9c484499e669d61f247aad76784362bf10532047
thecodingrunner <finn.moffett@gmail.com>
2024-05-29 20:19:45 +0100

    Vercel redeployment

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,9 @@
-import PreloadSteveImages from "@/components/PreloadImages";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <PreloadSteveImages/>
       <body>
         <Main />
         <NextScript />
